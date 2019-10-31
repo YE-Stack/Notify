379e5861e3db0ce94684874526b1d1722fd2ce78
krishnapsn <krishnapsnambiar@gmail.com>
2019-10-31 20:48:48 +0530

    Added front page

--- WebApp/views.py
+++ WebApp/views.py
@@ -5,6 +5,6 @@ from django.http import HttpResponse
 
 def home_view(request):
 	if request.method == 'GET':
-		return render(request, 'root.html')
+		return render(request, 'root.html',{"output":"	hello"})
 	else:
 		return HttpResponse(status=405)
