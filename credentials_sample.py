0612fbfd4fe65d8246d543d2ea66917093dba3d5
Abraham Francis <abfr049@gmail.com>
2019-11-02 16:44:45 +0530

    add a credentials sample config

---
+++ credentials_sample.py
@@ -0,0 +1,26 @@
+# Firebase Config
+firebase_config = {
+    "apiKey": "",
+    "authDomain": "",
+    "databaseURL": "",
+    "projectId": "",
+    "storageBucket": "",
+    "messagingSenderId": "",
+    "appId": ""
+}
+
+# Path to Firebase Admin Conifg JSON file
+path_to_json = ""
+
+# Twilio Authentication
+twilio_auth = ("Twilio Auth", "Twilio Contact")
+
+# Mobile Contact Numbers
+
+twilio_contact = "Twilio Contact Number for WhatsApp"
+
+twilio_sms_contact = "Twilio Contact Number for SMS"
+
+mobile_contact = "Mobile User Phone Number"
+
+web_contact = "Web App User Phone Number"
